6ce815d593e7bd92c241d58551647166d2b19a7c
Ciko Edo Febrian <ciko.edo@suitmedia.com>
2024-12-01 21:38:46 +0700

    fix: host issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
 
-
 export default [
-    daStyle,
-    {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-    {languageOptions: {globals: globals.node}},
-    pluginJs.configs.recommended,
+  daStyle,
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
 ];
